ec03de5409bf6d430bcb14e11102369f37f2f3ea
nombre <daniel.b100726@gmail.com>
2021-07-10 17:50:58 -0400

    Clase 7: Ventajas de la programación orientada a objetos, Reto de Crear clase learningPaths

--- index2.js
+++ index2.js
@@ -1,5 +1,39 @@
+class learningPaths {
+    constructor({
+        id,
+        name,
+        courses = [],
+        logo = "aquí debería ir una url para el logo",
+    }){
+        this.id = id
+        this.name = name
+        this.courses = courses
+        this.logo = logo
+    }
+}
+
+const escuelaDesarrolloWeb = new learningPaths({
+    id: 1,
+    name: "Escuela de Desarrollo Web",
+    courses: [
+        "Curso Gratuito de Programación",
+        "Curso Fundamentos de JavaScript",
+        "Curso de React JS"
+    ]
+})
+
+const escuelaDataScience = new learningPaths({
+    id: 2,
+    name: "Escuela de Data Science",
+    courses: [
+        "Curso Gratuito de Análisis de Negocios para Ciencia de Datos",
+        "Curso Básico de Python",
+        "Curso de Python Intermedio"
+    ]
+})
 class Student {
     constructor({
+        id,
         name,
         email, 
         userName,
@@ -9,6 +43,7 @@ class Student {
         approverCourses = [],
         learningPaths = [],
     }) {
+        this.id = id
         this.name = name
         this.email = email
         this.userName = userName
@@ -28,6 +63,14 @@ const juan2 = new Student({
     userName: "juancd10",
     email: "juan@platzi.com",
     twitter: "@juandc1010",
+    approverCourses: [
+        "Curso 1",
+        "Curso 2",
+        "Curso 3"
+    ],
+    learningPaths: [
+        escuelaDesarrolloWeb
+    ],
 })
 
 const miguel2 = new Student({
@@ -35,4 +78,12 @@ const miguel2 = new Student({
     userName: "migueld10",
     email: "miguel@platzi.com",
     instagram: "migueldc1010",
+    approverCourses: [
+        "Curso 4",
+        "Curso 5",
+        "Curso 6"
+    ],
+    learningPaths: [
+        escuelaDataScience
+    ],
 })
